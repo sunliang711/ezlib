9de548c9bb7369efb12e008a8a7a4b7f6231364d
sunliang <sunliang711@163.com>
2020-06-11 11:33:26 +0800

    buf len macro

--- src/common.h
+++ src/common.h
@@ -4,4 +4,12 @@
 #define max(a,b) (a) > (b) ? (a) : (b)
 
 
+#define BUF_LEN_255 255
+#define BUF_LEN_512 512
+#define BUF_LEN_1k 1024
+#define BUF_LEN_2k 2048
+#define BUF_LEN_4k 4096
+#define BUF_LEN_8k 8192
+
+
 #endif
