ecee3cb79fac7770b0411abdc3579bc20a6a3e1c
sunliang <sunliang711@163.com>
2020-06-11 13:54:02 +0800

    directory architecture

---
+++ ezlib/ezlib.h
@@ -0,0 +1,12 @@
+#ifndef __EZLIB_H__
+#define __EZLIB_H__
+
+#include "readline.h"
+#include "readn.h"
+#include "return.h"
+#include "socket_util.h"
+#include "writen.h"
+#include "memory.h"
+#include "common.h"
+
+#endif
